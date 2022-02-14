b3fef17dec1933ba20792b604143ff1a3962f56c
AnnaMelekh <annasmelekhina@gmail.com>
2022-02-15 01:47:50 +0300

    Initial Commit

---
+++ TrafficLightsSwiftUI/TrafficLightsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightsSwiftUIApp.swift
+//  TrafficLightsSwiftUI
+//
+//  Created by Anna Melekhina on 15.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
